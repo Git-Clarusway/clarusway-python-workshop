c18c932a02380fae09cdb1a2d2283a86bb4f6c85
Yaz <kaplan3260@gmail.com>
2020-07-03 23:42:48 -0500

    added flask 02 hands-on

--- flask-01-hello-world-app-on-ec2-linux2/hello-world app.py
+++ flask-01-hello-world-app-on-ec2-linux2/hello-world app.py
@@ -1,8 +1,13 @@
 #hello-world app.py
 from flask import Flask
+
 app=Flask(__name__)
+
 @app.route('/')
 def hello():
     return 'hello world'
+
 if __name__=='__main__':
     app.run(debug=True) 
+    # app.run('localhost',port=80)
+    app.run('0.0.0.0',port=80)
