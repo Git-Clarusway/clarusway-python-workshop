899c4d936a435e6e40a9c0c734720514f8328e63
Yaz <kaplan3260@gmail.com>
2020-07-12 12:24:33 -0500

    modified

--- flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling..py
+++ flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling..py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # send your name as template variable, and assign route of no path ('/')
 @app.route('/')
 def home():
-    return render_template('index.html', name='S_K')
+    return render_template('index.html', name='SK')
 
 # Write a function named `greet` which uses template file named `greet.html` given under `templates` folder
 # and assign to the static route of ('/greet')
